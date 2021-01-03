3b5f5548970306492266fae44dfd48f7e68f3922
“yz53665” <875423353@qq.com>
2021-01-03 23:54:57 +0800

    release

--- carSolve.py
+++ carSolve.py
@@ -3,33 +3,35 @@ from pyomo.environ import *
 from pyomo.dae import *
 
 m = ConcreteModel()
-cos = 0.5
-sin = 0.866
+cos = 0.985 # 10度
+sin = 0.174
+T = 60
 
 m.mu = Param(initialize=0.0040) # 摩擦系数 
 m.L = Param(initialize=100.0) # 最后的位置
 m.C = Param(initialize=0.35) # 空气阻力系数
-m.S = Param(initialize=2.05) # 迎风面积
+m.S = Param(initialize=2.26) # 迎风面积
 m.g = Param(initialize=9.8)
-m.m = Param(initialize=1560)
+m.m = Param(initialize=1560) # 1.5吨
 m.cosTheta = Param(initialize=cos)
 m.sinTheta = Param(initialize=sin)
-m.rho = Param(initialize=1.293) # 空气密度
+m.delta = Param(initialize=1.2) # 加速阻力的旋转质量换算系数
+m.rho = Param(initialize=1.22558) # 空气密度
 
 m.tau = ContinuousSet(bounds=(0,1)) #Unscaled time 
 m.time = Var(m.tau) #Scaled time 
-m.tf = Var()
+m.tf = Var(bounds=(0, T))
 m.x = Var(m.tau,bounds=(0,None)) 
-m.v = Var(m.tau,bounds=(0,30))
+m.v = Var(m.tau,bounds=(0,None))
 m.a = Var(m.tau,bounds=(-3.0,1.0),initialize=0)
-m.P = Var(m.tau,bounds=(0,205800),initialize=0)
+m.F = Var(m.tau)
 m.dtime = DerivativeVar(m.time) 
 m.dx = DerivativeVar(m.x) 
 m.dv = DerivativeVar(m.v)
 
 
 def _intJ(m, t):
-    return m.P[t]
+    return m.F[t] * m.v[t]
 m.intJ = Integral(m.tau, wrt=m.tau, rule=_intJ)
 
 def _odel(m,t):
@@ -53,17 +55,11 @@ m.ode3 = Constraint(m.tau, rule=_ode3)
 def _ode4(m,t):
     if t == 0:
         return Constraint.Skip 
-    return m.a[t] == (m.P[t] / (m.m*m.v[t]) - \
-                    m.cosTheta * m.mu * m.g - \
-                    m.sinTheta * m.g - \
-                    m.C * m.rho * m.S * m.v[t]**2 * 0.5 / m.m)
+    return m.F[t] == m.C * m.rho * m.S * m.v[t]**2 * 0.5 + \
+                    (m.cosTheta * m.mu * m.g + \
+                    m.sinTheta * m.g + \
+                    m.delta * m.a[t]) * m.m
 m.ode4 = Constraint(m.tau, rule=_ode4)
-
-def _ode5(m,t):
-    if t == 0:
-        return Constraint.Skip
-    return m.P[t] == m.m * m.v[t]
-m.ode5 = Constraint(m.tau, rule=_ode5)
 
 def _init(m):
     yield m.x[0] == 0 
@@ -80,14 +76,15 @@ discretizer.apply_to(m,nfe=15, scheme='BACKWARD')
 solver = SolverFactory('ipopt') 
 solver.solve(m, tee=False)
 
-print("final time = %6.2f" %(value(m.intJ)))
+print("Spent energy= %6.2f" %(value(m.intJ)))
+print("Spent tiem= %6.2f" %(value(m.tf)))
 
 import matplotlib.pyplot as plt
 
 x=[] 
 v=[] 
 a=[] 
-P=[]
+F=[]
 time=[]
 
 for i in m.tau:
@@ -96,7 +93,7 @@ for i in m.tau:
     x.append(value(m.x[i])) 
     v.append(value(m.v[i])) 
     a.append(value(m.a[i]))
-    P.append(value(m.P[i]))
+    F.append(value(m.F[i]))
 
 plt.plot(time,x,label='x') 
 plt.title('location') 
@@ -113,7 +110,3 @@ plt.title('acceleration')
 plt.xlabel('time') 
 plt.show()
 
-plt.plot(time,P,label='P') 
-plt.title('P') 
-plt.xlabel('time') 
-plt.show()
---
+++ plot.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+plt.plot([40, 60, 80, 150, 300], [6930, 4572, 3416, 1816, 907])
+plt.xlabel('time')
+plt.ylabel('Energy')
+plt.show()
